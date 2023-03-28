a71a15f62dd38b2f33fdf8d92ce8aeafbded7886
Daniel Bui 3 <129134803+Sotatek-DanielBui3@users.noreply.github.com>
2023-03-28 15:17:19 +0700

    Update Program.cs

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -23,12 +23,8 @@ builder.Services.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<A
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
